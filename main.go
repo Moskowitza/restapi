d11eea41b4609de472385c0c7b6db27a9a3f190b
moskowitza <moskowitzaaron@mac.com>
2019-11-27 13:26:19 -0500

    fix update

--- main.go
+++ main.go
@@ -66,7 +66,7 @@ func updateBook(w http.ResponseWriter, r *http.Request) {
 			books = append(books[:index], books[index+1:]...)
 			var book Book
 			_ = json.NewDecoder(r.Body).Decode(&book)
-			book.ID = strconv.Itoa(rand.Intn(10000000)) //MockID
+			book.ID = params["id"] //MockID
 			books = append(books, book)
 			json.NewEncoder(w).Encode(book)
 			return
